96b1a70b7d9b8e05e808cb34cce5971151ce32ae
GitHub Action <automation@noclick.me>
2021-03-09 14:11:14 +0000

    Build for noclick-me/noclick.me@refs/heads/main (9f4b26f211f78a32d5fb843c2f6324f1b8ab4260)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,8 +16,8 @@ const RESOURCES = {
 "version.json": "4617b344484c27f9cdf6c2177f9842cb",
 "manifest.json": "a77efe50f0f1f9aeb832bddc1764ee9d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "5698a1c1ec88dd841ec05846626f7289",
-"/": "5698a1c1ec88dd841ec05846626f7289"
+"index.html": "f9e9642c6398eab900087ba7d990fca5",
+"/": "f9e9642c6398eab900087ba7d990fca5"
 };
 
 // The application shell files that are downloaded before a service worker can
